665cb7c1b41eeb7f8583016c236a7c4ec5fc56d0
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-03-24 21:27:12 +0300

    Add project files.

---
+++ Arrays/Source.cpp
@@ -0,0 +1,68 @@
+//Arrays
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5; //количество элементов массива (размер массива)  
+	int arr[n] = {};
+	int sum = 0, a = 0;
+	double x = 0;
+	/*arr[2] = 123;*/
+
+	cout << "Введите элементы массива (" << n << " шт):";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Вывод массива на экран в обратном порядке:
+	for (int i = n - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Вывод суммы элементов массива
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum << endl;
+
+	//Вывод средней арифметической
+	cout << "Среднее арифметическое массива: " << (double)x/n << endl;
+
+	//Вывод минимального значения
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+
+	//Вывод максимального значения
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max) max = arr[i];
+	}
+	cout << "Максимальное значение в массиве: " << max << endl;
+
+//Поиск минимального и максимального значения:
+	min = max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+		if (arr[i] > max) max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
+}
