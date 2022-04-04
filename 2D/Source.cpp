0224032faa6046d53f066890b4a5ecc0e6db620b
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-04-05 00:12:51 +0300

    determinant

--- 2D/Source.cpp
+++ 2D/Source.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 #define tab "\t"
+
+//#define TWO
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int ROWS = 8; //количество строк
-	const int COLS = 5; //количество столбцов (элементов строки)
-	int arr[ROWS][COLS] =
-	{
-		{1,2,3},
-		{4,5,6},
+#ifdef TWO	
+	const int ROWS = 5; //количество строк i
+	const int COLS = 5; //количество столбцов (элементов строки) j
+	int arr[ROWS][COLS] = {
+		{1, 2, 3},
+		{ 4,5,6 },
 		{},
-		{7,8,9}
-	};
+		{ 7,8,9 } };
 	for (int i = 0; i < ROWS; i++)
 	{
 		for (int j = 0; j < COLS; j++)
@@ -30,4 +31,103 @@ void main()
 		}
 		cout << endl;
 	}
+#endif // TWO
+	//int n, m;
+	//const int ROWS = 2; //количество строк i
+	//const int COLS = 5; //количество столбцов (элементов строки) j
+	//int A[ROWS][COLS] = {};
+	//int B[ROWS][COLS] = {};
+	//for (int i = 0; i < ROWS; i++)
+	//{
+	//	for (int j = 0; j < COLS; j++) //заполнение матрицы случайными числами
+	//	{
+	//		A[i][j] = rand() % 10;
+	//	}
+	//	for (int j = 0; j < COLS; j++) //дублирование матрицы А в матрицу В
+	//	{
+	//		B[i][j] = A[i][j];
+	//	}
+	//}
+
+
+	//for (int i = 0; i < ROWS; i++)
+	//{
+	//	for (int j = 0; j < COLS; j++) // перебирает элементы строки
+	//	{
+	//		cout << A[i][j] << tab;
+	//	}cout << endl;
+	//}
+	//cout << endl;
+	//for (int i = 0; i < ROWS; i++)
+	//{
+	//	for (int j = 0; j < COLS; j++) // перебирает элементы строки
+	//	{
+	//		cout << B[i][j] << tab;
+	//	}cout << endl;
+	//}
+
+
+
+
+
+
+
+
+	int n, m;
+	const int ROWS = 2; //количество строк i
+	const int COLS = 5; //количество столбцов (элементов строки) j
+	int arr[ROWS][COLS] = {};
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++) //столбцы
+		{
+			arr[i][j] = rand() % 10;
+
+			for (int n = 0; n < i; n++)
+			{
+				for (int m = 0; m < j; m++) //столбцы
+				{
+					if (arr[i][j] == arr[n][m])
+					{
+						i--;
+						n--;
+						break;
+						
+					}
+				}
+			}
+		}
+	}
+	for (int i = 0; i < ROWS; i++) //перебирает строки 
+	{
+		for (int j = 0; j < COLS; j++) // перебирает элементы строки
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+
+
+	/*for (int i = 0; i < n; i++) //строки
+	{
+		arr[i] = rand() % n;
+		for (int j = 0; j <  COLS; j++) //столбцы
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+*/
+
 }
--- Determinant/Source.cpp
+++ Determinant/Source.cpp
@@ -1,10 +1,97 @@
 #include<iostream>
 using namespace std;
 
-#define tab "\n"
+#define tab "\t"
+#define delimiter "\n----------------------------------\n"
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int m = 3; //количество строк
+	const int n = 3; //количество столбцов
+	int A[m][n] = {};
+	//int B[m][n] = {};
+	//int x = 0, k = 1, k1 = 1, k2 = 1, y = 0;
 
+	//заполняем матрицу 
+	//for (int i = 0; i < m; i++)
+	//{
+	//	for (int j = 0; j < n + n - 1; j++) // увеличиваем количество столбцов
+	//	{
+	//		if (j < n) //заполнение матрицы случайными числами
+	//		{
+	//			A[i][j] = rand() % 20;
+	//			cout << A[i][j] << tab;
+	//		}
+	//		else if (j >= n) //расширяем матрицу (дублирование первых столбцов в конце)
+	//		{
+	//			if (y == n - 1) { y = 0; x++; }
+	//			else if (y > n - 2) x++;
+	//			A[i][j] = A[x][y];
+	//			y++;
+	//			cout << A[i][j] << tab;
+	//		}
+	//	}	cout << endl;
+	//}
+
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n; j++) A[i][j] = rand() % 10;
+	}
+	/*for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << A[i][j] << "\t";
+		}
+		cout << endl;
+	}*/
+
+	int B[n][n * 2];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) B[i][j] = B[i][j + n] = A[i][j];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n * 2 - 1; j++) cout << B[i][j] << "\t";
+		cout << endl;
+	}
+	cout << delimiter << endl;
+
+	int sum_glavnaya_diag = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		int ymnojenie = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (B[j][j + i] == 0) B[j][j + i] = 1;
+			ymnojenie *= B[j][j + i];
+		}
+		sum_glavnaya_diag += ymnojenie;
+	}
+	cout << "Сумма главной диагонали = " << sum_glavnaya_diag << endl;
+	
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int ymnojenie = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (B[n - j - 1][j + i]==0) B[n - j - 1][j + i] = 1;
+			ymnojenie *= B[n - j - 1][j + i];
+		}
+		sum += ymnojenie;
+	}
+	cout << "Сумма вспомогательной диагонали = " << sum << endl;
+	int det = sum_glavnaya_diag - sum;
+	cout << "Детерминант матрицы методом Саррюса " << det << endl;
 }
+
+
+
+
+
+
