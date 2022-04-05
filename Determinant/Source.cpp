ac53dbbb5760d672ea08793b057bfb7654a6637c
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-04-05 21:42:09 +0300

    2D arrays day 2

--- Determinant/Source.cpp
+++ Determinant/Source.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 #define tab "\t"
 #define delimiter "\n----------------------------------\n"
+//#define DET
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int m = 3; //количество строк
-	const int n = 3; //количество столбцов
-	int A[m][n] = {};
+	const int n = 3;
+	//const int n = 3;
+	int A[n][n] =
+	{
+		{1,2,3},
+		{4,5,6},
+		{7,8,9}
+	};
+#ifdef DET
+	const int m = 3;
 	//int B[m][n] = {};
 	//int x = 0, k = 1, k1 = 1, k2 = 1, y = 0;
 
@@ -34,7 +43,7 @@ void main()
 	//	}	cout << endl;
 	//}
 
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++) A[i][j] = rand() % 10;
 	}
@@ -61,26 +70,26 @@ void main()
 	cout << delimiter << endl;
 
 	int sum_glavnaya_diag = 0;
-	
+
 	for (int i = 0; i < n; i++)
 	{
 		int ymnojenie = 1;
 		for (int j = 0; j < n; j++)
 		{
-			if (B[j][j + i] == 0) B[j][j + i] = 1;
+			//if (B[j][j + i] == 0) B[j][j + i] = 1;
 			ymnojenie *= B[j][j + i];
 		}
 		sum_glavnaya_diag += ymnojenie;
 	}
 	cout << "Сумма главной диагонали = " << sum_glavnaya_diag << endl;
-	
+
 	int sum = 0;
 	for (int i = 0; i < n; i++)
 	{
 		int ymnojenie = 1;
 		for (int j = 0; j < n; j++)
 		{
-			if (B[n - j - 1][j + i]==0) B[n - j - 1][j + i] = 1;
+			//if (B[n - j - 1][j + i]==0) B[n - j - 1][j + i] = 1;
 			ymnojenie *= B[n - j - 1][j + i];
 		}
 		sum += ymnojenie;
@@ -88,6 +97,68 @@ void main()
 	cout << "Сумма вспомогательной диагонали = " << sum << endl;
 	int det = sum_glavnaya_diag - sum;
 	cout << "Детерминант матрицы методом Саррюса " << det << endl;
+#endif // DET
+	//for (int i = 0; i < n; i++)
+	//{
+	//	for (int j = 0; j < n; j++)
+	//	{
+	//		A[i][j] = rand() % 10;
+	//	}
+	//}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << A[i][j] << tab;
+		}
+		cout << endl;
+	}
+
+	//Создаем расширенную матрицу:
+	int B[n][n * 2] = {};
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			B[i][j] = B[i][j + n] = A[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n * 2; j++)
+		{
+			cout << B[i][j] << tab;
+		}
+		cout << endl;
+	}
+	//Вычисляем определитель
+
+	//Вычисляем главную диагональ:
+	int main_diag = 0;
+	for (int i = 0; i < n; i++)
+	{
+		int product = 1; //произведение элементов главной диагонали
+		for (int j = 0; j < n; j++)
+		{
+			product *= B[j][i + j];
+		}
+		main_diag += product;
+	}
+	cout << "Главная диагональ: " << main_diag << endl;
+
+	//Вычисляем вспомогательную диагональ:
+	int aux_diag = 0; //aux-Auxilliary (Вспомогательный)
+	for (int i = 0; i < n; i++)
+	{
+		int product = 1;
+		for (int j = 0; j < n; j++)
+		{
+			product *= B[n - 1 - j][i + j];
+		}
+		aux_diag += product;
+	}
+	cout << "Вспомогательная диагональ: " << aux_diag << endl;
+	cout << "Определитель матрицы: " << main_diag - aux_diag << endl;
 }
 
 
---
+++ UniqueRand2/Source.cpp
@@ -0,0 +1,45 @@
+//UniqueRand2
+#include<iostream>
+using namespace std;
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int ROWS = 4;
+	const int COLS = 5;
+	int arr[ROWS][COLS] = {};
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			bool unique = true; //предполагаем, что случайное число будет уникальным
+			do
+			{
+				arr[i][j] = rand() % (ROWS*COLS);
+				unique = true; //предполагаем, что случайное число будет уникальным
+				//но это нужно проверить:
+				for (int k = 0; k <= i; k++)
+				{
+					for (int l = 0; l < (k == i ? j : COLS); l++)
+					{
+						if (arr[i][j] == arr[k][l])
+						{
+							unique = false;
+							break;
+						}
+					}
+					if (!unique)break;
+				}
+			} while (!unique);
+		}
+	}
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
